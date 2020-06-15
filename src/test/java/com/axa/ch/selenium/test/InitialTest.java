2391085bb2cb21e45a9d1b8b7bc75480edc604cc
mimi <michel@hostettler-edv.ch>
2020-06-15 07:43:21 +0200

    New Config Keys added to Toolkit. "target.env" to set a target-Stage. Is this property setted in main data-File it will be used for getting the detailed File. env can now be setted with "exec.env" too.
    New Beta Datascript-Executor with resultset and dataextractor

--- src/test/java/com/axa/ch/selenium/test/InitialTest.java
+++ src/test/java/com/axa/ch/selenium/test/InitialTest.java
@@ -2,6 +2,7 @@ package com.axa.ch.selenium.test;
 
 import com.axa.ch.selenium.pageObject.KontaktPageObject;
 import com.axa.ch.selenium.pageObject.StartseitePageObject;
+import org.openqa.selenium.support.events.EventFiringWebDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -13,11 +14,13 @@ import java.util.concurrent.TimeUnit;
 
 public class InitialTest {
 
-    @Test(groups = {"allTests", "initTests"}, seleniumTest = true, testName = "AntragListeTest")
+    @Test(groups = {"allTests", "initTests"}, seleniumTest = true, testName = "AntragListeTest", nlUserPath = "asfadf")
     @Feature("InitialTest other depends on this")
     public void webseiteVerfügbar(){
         WebDriverManager.getWebdriver().manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+        WebDriverManager.startNLTransaction("Michel");
         AbstractSeleniumConsultingPageObject.loadSeleniumConsultingPage();
+        WebDriverManager.stopNLTransaction();
         Assert.assertEquals(WebDriverManager.getWebdriver().getTitle(), "Home - Selenium-Consulting - Gerne für Sie und Ihre Zukunft da");
     }
 
--- src/test/java/com/axa/ch/selenium/test/SampleTest.java
+++ src/test/java/com/axa/ch/selenium/test/SampleTest.java
@@ -3,13 +3,14 @@ package com.axa.ch.selenium.test;
 import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 
+import seleniumConsulting.ch.selenium.framework.dataLoader.TestDataProvider;
 import seleniumConsulting.ch.selenium.framework.driver.WebDriverManager;
 
 public class SampleTest {
 
     @Test(groups = {"allTests"}, seleniumTest = true)
-    @Ignore
     public void sample() {
         WebDriverManager.getWebdriver().get("http://www.google.ch");
+        System.out.println(TestDataProvider.getTestData("startUrl"));
     }
 }
